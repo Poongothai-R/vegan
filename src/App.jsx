41e14535d00a28cf89c5e95cbaab63b2552fa11d
Poongothai-R <107067696+Poongothai-R@users.noreply.github.com>
2023-01-18 10:21:21 +0100

    initial setup and created all components

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './styles/global/App.css';
+import './styles/global/variables.css';
+import Navbar from './components/Navbar';
+import Home from './Pages/Home';
+import Footer from './components/Footer';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Category from './Pages/Category';
+import Product from './Pages/Product';
+import Contact from './Pages/Contact';
+
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            {/* <div className="App"> */}
+
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:Category" element={<Category />} />
+                <Route path="/:Category/:id" element={<Product />} />
+                <Route path="/Contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+
+            {/* </div>  */}
+        </BrowserRouter>
+    );
+};
