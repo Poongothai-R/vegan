c6271e03e1e07ef42a2a5718c6d98ac457575923
Poongothai-R <107067696+Poongothai-R@users.noreply.github.com>
2023-01-19 22:27:14 +0100

    done real map embedding.. fix css issues in form

--- src/components/LocationMap.jsx
+++ src/components/LocationMap.jsx
@@ -5,7 +5,7 @@ export default function LocationMap() {
 
     return (
         <div className='map'>
-            <iframe className="map-page" title='VeganRestaurant' frameBorder="0" loading="loading..."
+            <iframe className="map-page" title='VeganRestaurant'  loading="loading..."
                     src="https://maps.google.com/maps?hl=en&amp;q=stallarholmen&amp;t=k&amp;z=17&amp;
                     ie=UTF8&amp;iwloc=B&amp;output=embed">
             </iframe>
